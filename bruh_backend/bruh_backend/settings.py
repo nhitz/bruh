9feb76a029d9669502957cd50a8c3ae624a717f8
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-12 15:17:30 -0800

    settup account model

--- bruh_backend/bruh_backend/settings.py
+++ bruh_backend/bruh_backend/settings.py
@@ -15,6 +15,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'account.User'
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=180),
