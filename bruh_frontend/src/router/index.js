f583743aaad1aa4bf447c38bf75493c03b471766
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-12 13:40:17 -0800

    add MessagesView.vue page

--- bruh_frontend/src/router/index.js
+++ bruh_frontend/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import SignupView from "@/views/SignupView.vue";
 import LoginView from "@/views/LoginView.vue";
 import FeedView from "@/views/FeedView.vue";
+import MessagesView from "@/views/MessagesView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,10 +22,16 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: LoginView
-        }, {
+        },
+        {
             path: '/feed',
             name: 'feed',
             component: FeedView
+        },
+        {
+            path: '/messages',
+            name: 'messages',
+            component: MessagesView
         },
         {
             path: '/about',
