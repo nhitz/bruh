b98892e30155f95e6b9c73c7aec8027bb2d4e204
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-14 08:49:06 -0800

    F

--- bruh_frontend/src/views/SignupView.vue
+++ bruh_frontend/src/views/SignupView.vue
@@ -5,42 +5,50 @@
         <h1 class="mb-6 text-2xl">Sign up</h1>
 
         <p class="mb-6 text-gray-500">
-          Don't have an account?
-          <RouterLink :to="{'name': 'login'}" class="underline">Click here to sign up!</RouterLink>
+          Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate.
+          Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate.
         </p>
 
-        <!--                <p class="font-bold">-->
-        <!--                    Already have an account? <RouterLink :to="{'name': 'login'}" class="underline">Click here</RouterLink> to log in!-->
-        <!--                </p>-->
+        <p class="font-bold">
+          Already have an account?
+          <RouterLink :to="{'name': 'login'}" class="underline">Click here</RouterLink>
+          to log in!
+        </p>
       </div>
     </div>
 
     <div class="main-right">
       <div class="p-12 bg-white border border-gray-200 rounded-lg">
-        <form class="space-y-6">
+        <form class="space-y-6" v-on:submit.prevent="submitForm">
           <div>
             <label>Name</label><br>
-            <input type="text" placeholder="Your full name"
+            <input type="text" v-model="form.name" placeholder="Your full name"
                    class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
           </div>
 
           <div>
             <label>E-mail</label><br>
-            <input type="email" placeholder="Your e-mail address"
+            <input type="email" v-model="form.email" placeholder="Your e-mail address"
                    class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
           </div>
 
           <div>
             <label>Password</label><br>
-            <input type="password" placeholder="Your password"
+            <input type="password" v-model="form.password1" placeholder="Your password"
                    class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
           </div>
 
           <div>
             <label>Repeat password</label><br>
-            <input type="password" placeholder="Repeat your password"
+            <input type="password" v-model="form.password2" placeholder="Repeat your password"
                    class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
           </div>
+
+          <template v-if="errors.length > 0">
+            <div class="bg-red-300 text-white rounded-lg p-6">
+              <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+            </div>
+          </template>
 
           <div>
             <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Sign up</button>
@@ -52,55 +60,54 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
+
+import {useToastStore} from '@/stores/toast'
 
 export default {
   setup() {
-    const toastStore = userToastStore();
+    const toastStore = useToastStore()
 
     return {
       toastStore
     }
-  }
+  },
 
   data() {
     return {
       form: {
-        name: '',
         email: '',
-        password: '',
-        password_confirmation: ''
+        name: '',
+        password1: '',
+        password2: ''
       },
-      errors: []
+      errors: [],
     }
-  }
+  },
 
   methods: {
     submitForm() {
-      this.errors = [];
-
-      if (this.form.name === '') {
-        this.errors.push('Name is required');
-      }
+      this.errors = []
 
       if (this.form.email === '') {
-        this.errors.push('Email is required');
+        this.errors.push('Your e-mail is missing')
       }
 
-      if (this.form.password === '') {
-        this.errors.push('Password is required');
+      if (this.form.name === '') {
+        this.errors.push('Your name is missing')
       }
 
-      if (this.form.password_confirmation === '') {
-        this.errors.push('Password confirmation is required');
+      if (this.form.password1 === '') {
+        this.errors.push('Your password is missing')
       }
 
-      if (this.form.password !== this.form.password_confirmation) {
-        this.errors.push('Passwords do not match');
+      if (this.form.password1 !== this.form.password2) {
+        this.errors.push('The password does not match')
       }
 
       if (this.errors.length === 0) {
-        axios.post('/api/signup', this.form)
+        axios
+            .post('/api/signup/', this.form)
             .then(response => {
               if (response.data.message === 'success') {
                 this.toastStore.showToast(5000, 'The user is registered. Please log in', 'bg-emerald-500')
@@ -112,6 +119,9 @@ export default {
               } else {
                 this.toastStore.showToast(5000, 'Something went wrong. Please try again', 'bg-red-300')
               }
+            })
+            .catch(error => {
+              console.log('error', error)
             })
       }
     }
