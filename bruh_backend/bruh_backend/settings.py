a1e34f3c800664f14c3b32a443c3615ea15ddd7f
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-14 12:45:44 -0800

    update dev cors policy

--- bruh_backend/bruh_backend/settings.py
+++ bruh_backend/bruh_backend/settings.py
@@ -33,11 +33,11 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 INSTALLED_APPS = [
