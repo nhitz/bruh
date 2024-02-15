6e89b1619213d0f89d75a074f99948dfa4e6fdb8
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-15 10:17:00 -0800

    r

--- bruh_backend/post/api.py
+++ bruh_backend/post/api.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import api_view
 
 from .forms import PostForm
 from .models import Post
