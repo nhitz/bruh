5bdda714e96d684685ed0995d3398589948c3754
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-15 10:14:06 -0800

    post_create()

--- bruh_backend/post/api.py
+++ bruh_backend/post/api.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
+from .forms import PostForm
 from .models import Post
 from .serializers import PostSerializer
 
@@ -9,14 +10,23 @@ from .serializers import PostSerializer
 @api_view(['GET'])
 def post_list(request):
     posts = Post.objects.all()
+
     serializer = PostSerializer(posts, many=True)
+
     return JsonResponse(serializer.data, safe=False)
 
 
 @api_view(['POST'])
 def post_create(request):
-    data = request.data
+    form = PostForm(request.data)
 
-    print(data)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
 
-    return JsonResponse({'message': 'Post created successfully'}, status=201)
+        serializer = PostSerializer(post)
+
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        return JsonResponse({'error': 'add somehting here later!...'})
