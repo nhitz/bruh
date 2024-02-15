e5b02580e9eca73bb4622436c192e56a6e6ed9ec
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-15 09:36:02 -0800

    create posts endpoint

---
+++ bruh_backend/post/api.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+@api_view(['GET'])
+def post_list(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return JsonResponse({'data': serializer.data})
--- bruh_backend/post/models.py
+++ bruh_backend/post/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class PostAttachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to='post_attachments')
+    created_by = models.ForeignKey(User, related_name='post_attachments', on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    body = models.TextField(blank=True, null=True)
+
+    attachments = models.ManyToManyField(PostAttachment, related_name='posts', blank=True)
+    # likes
+    # likes_count
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
