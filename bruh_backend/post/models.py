bd0722949d7d428ee1a958bcc82054d03ee8b2ec
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-15 09:49:05 -0800

    post ordering“

--- bruh_backend/post/models.py
+++ bruh_backend/post/models.py
@@ -21,3 +21,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
