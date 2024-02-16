3c8d69793d02bfeb5c575e1b8f09d38e86607f4b
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-16 14:01:19 -0800

    run black

--- bruh_backend/account/api.py
+++ bruh_backend/account/api.py
@@ -1,31 +1,39 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 
 from .forms import SignupForm
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def me(request):
-    return JsonResponse({
-        'id': request.user.id,
-        'name': request.user.name,
-        'email': request.user.email,
-    })
+    return JsonResponse(
+        {
+            "id": request.user.id,
+            "name": request.user.name,
+            "email": request.user.email,
+        }
+    )
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @authentication_classes([])
 @permission_classes([])
 def signup(request):
     data = request.data
-    message = 'success'
+    message = "success"
 
-    form = SignupForm({
-        'email': data.get('email'),
-        'name': data.get('name'),
-        'password1': data.get('password1'),
-        'password2': data.get('password2'),
-    })
+    form = SignupForm(
+        {
+            "email": data.get("email"),
+            "name": data.get("name"),
+            "password1": data.get("password1"),
+            "password2": data.get("password2"),
+        }
+    )
 
     if form.is_valid():
         form.save()
@@ -34,4 +42,4 @@ def signup(request):
     else:
         message = form.errors
 
-    return JsonResponse({'message': message})
+    return JsonResponse({"message": message})
--- bruh_backend/account/models.py
+++ bruh_backend/account/models.py
@@ -18,21 +18,21 @@ class CustomUserManager(UserManager):
         return user
 
     def create_user(self, name=None, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
         return self._create_user(name, email, password, **extra_fields)
 
     def create_superuser(self, name=None, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
         return self._create_user(name, email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255, blank=True, default='')
-    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, default="")
+    avatar = models.ImageField(upload_to="avatars", blank=True, null=True)
 
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
@@ -43,6 +43,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
     REQUIRED_FIELDS = []
