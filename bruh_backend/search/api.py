54fe71d68608858c72d32e7e56be2e7a42afa9fd
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-16 14:22:52 -0800

    search not post

--- bruh_backend/search/api.py
+++ bruh_backend/search/api.py
@@ -1,48 +1,22 @@
 from account.models import User
 from account.serializers import UserSerializer
 from django.http import JsonResponse
-from rest_framework.decorators import (
-    api_view,
-)
-
-from .forms import PostForm
-from .models import Post
-from .serializers import PostSerializer
-
-
-@api_view(["GET"])
-def post_list(request):
-    posts = Post.objects.all()  # Change later to feed
-
-    serializer = PostSerializer(posts, many=True)
-
-    return JsonResponse(serializer.data, safe=False)
-
-
-@api_view(["GET"])
-def post_list_profile(request, id):
-    user = User.objects.get(pk=id)
-    posts = Post.objects.filter(created_by_id=id)
-
-    posts_serializer = PostSerializer(posts, many=True)
-    user_serializer = UserSerializer(user)
-
-    return JsonResponse(
-        {"posts": posts_serializer.data, "user": user_serializer.data}, safe=False
-    )
+from post.models import Post
+from post.serializers import PostSerializer
+from rest_framework.decorators import api_view
 
 
 @api_view(["POST"])
-def post_create(request):
-    form = PostForm(request.data)
+def search(request):
+    data = request.data
+    query = data["query"]
 
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.created_by = request.user
-        post.save()
+    users = User.objects.filter(name__icontains=query)
+    users_serializer = UserSerializer(users, many=True)
 
-        serializer = PostSerializer(post)
+    posts = Post.objects.filter(body__icontains=query)
+    posts_serializer = PostSerializer(posts, many=True)
 
-        return JsonResponse(serializer.data, safe=False)
-    else:
-        return JsonResponse({"error": "add somehting here later!..."})
+    return JsonResponse(
+        {"users": users_serializer.data, "posts": posts_serializer.data}, safe=False
+    )
