08315929efe15fc15c612be3c2f1d19d1f869d18
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-16 14:04:02 -0800

    run black

--- bruh_backend/post/urls.py
+++ bruh_backend/post/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path('', api.post_list, name='post_list'),
-    path('profile/<uuid:id>/', api.post_list_profile, name='post_list_profile'),
-    path('create/', api.post_create, name='post_create'),
+    path("", api.post_list, name="post_list"),
+    path("profile/<uuid:id>/", api.post_list_profile, name="post_list_profile"),
+    path("create/", api.post_create, name="post_create"),
 ]
