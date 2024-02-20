56c55e7d1b061d875673e83d54fc88eecb41ef23
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-02-20 12:52:47 -0800

    create likes fields in post model

--- bruh_backend/post/models.py
+++ bruh_backend/post/models.py
@@ -1,8 +1,15 @@
 import uuid
 
-from account.models import User
 from django.db import models
 from django.utils.timesince import timesince
+
+from account.models import User
+
+
+class Like(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_by = models.ForeignKey(User, related_name="likes", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class PostAttachment(models.Model):
@@ -19,8 +26,8 @@ class Post(models.Model):
 
     attachments = models.ManyToManyField(PostAttachment, blank=True)
 
-    # likes
-    # likes_count
+    likes = models.ManyToManyField(Like, blank=True)
+    likes_count = models.IntegerField(default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
